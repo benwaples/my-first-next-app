6533b5132ba68269d7c36cd6d8e2e9d896befc99
benwaples <benwaples@gmail.com>
2021-03-10 15:40:53 -0800

    styled detailed page

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -24,7 +24,7 @@ function DetailCharacter({ character }: { character: CharacterTypes }) {
    <section className={styles.detailCharacter}>
      <h1>{name}</h1>
      <img src={image} alt={name}/>
-     <p>Description: {name} is {height}m tall, born on {bornLocation}, and is of {species} species. {capitalize(inaccuratePronouns(gender)[0])} has a {cybernetics} cybernetic. {name}'s masters include {commaSeparatedString(masters)}, and {inaccuratePronouns(gender)[2]} apprentices are {commaSeparatedString(apprentices)}.</p>
+     <p><strong>Description</strong>: {name} is {height}m tall, born on {bornLocation}, and is of {species} species. {capitalize(inaccuratePronouns(gender)[0])} has a {cybernetics} cybernetic. {name}'s masters include {commaSeparatedString(masters)}, and {inaccuratePronouns(gender)[2]} apprentices are {commaSeparatedString(apprentices)}.</p>
      <p>Click <a href={wiki} target="_blank">here</a> to learn more</p>
    </section>
   )
