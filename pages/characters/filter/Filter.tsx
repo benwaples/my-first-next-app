6562815202cc3aebc1e0b968b3c71c6e8b1d9e6d
benwaples <benwaples@gmail.com>
2021-03-11 10:41:27 -0800

    filter through characters

---
+++ pages/characters/filter/Filter.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { CharacterTypes } from '../../types'
+
+interface Props {
+  filterQuery: string;
+  setFilterQuery: (q: string) => void;
+}
+
+function Filter(props: Props) {
+  const {
+    filterQuery,
+    setFilterQuery
+  } = props
+
+  return (
+    <input type="text" name="filterQuery" id="filterQuery" value={filterQuery} onChange={(e) => setFilterQuery(e.target.value)}/> 
+  )
+}
+
+export default Filter
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,18 +1,34 @@
-
+import { useState, useEffect } from 'react'
 import { CharacterTypes } from '../types'
 import Character from './character'
 import styles from '../../styles/CharacterList.module.css'
+import Filter from './filter/Filter'
 
 
-function Index({ characters }): JSX.Element {
+function Index({ json }): JSX.Element {
+  const [characters, setCharacters] = useState<CharacterTypes[]>(json)
+  const [filteredCharacters, setFilteredCharacters] = useState<CharacterTypes[]>(json)
 
-  const characterEls: JSX.Element[] = characters.map((character: CharacterTypes, i: number) => <Character {...{character}}/>)
+  const [filterQuery, setFilterQuery] = useState<string>('')
 
+  useEffect(() => {
+    setFilteredCharacters(filterQuery ? characters.filter((character: CharacterTypes) => character.name.includes(filterQuery)) : characters) 
+
+  }, [filterQuery])
+
+  const characterEls: JSX.Element[] = filteredCharacters.map((character: CharacterTypes, i: number) => <Character {...{character}}/>)
+
+  
 
   return (
+    <>
+    <div>
+      <Filter {...{ filterQuery, setFilterQuery }}/>
+    </div>
     <ul className={styles.characterList}>
       {characterEls}
     </ul>
+    </>
   )
 }
 
@@ -24,6 +40,6 @@ export async function getStaticProps() {
   const request = await fetch('https://akabab.github.io/starwars-api/api/all.json')
   const json = await request.json()
   return ({
-    props: { characters: json }
+    props: { json }
   })
 }
