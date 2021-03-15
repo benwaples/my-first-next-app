197fa71981a8c35ce83ca20ba62592f3f358014d
benwaples <benwaples@gmail.com>
2021-03-15 13:53:15 -0700

    perPage optional parameter

--- pages/api/starwars/index.ts
+++ pages/api/starwars/index.ts
@@ -5,22 +5,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const page = Number(req.query.page)
   const perPage = Number(req.query.perPage)
-  console.log(page, perPage);
-  
   
   const request = await fetch('https://akabab.github.io/starwars-api/api/all.json')
   const json = await request.json() 
 
   if(page) {
-    const paginatedJson = makePages(json, perPage)
-
+    const paginatedJson = perPage ? makePages(json, perPage) : makePages(json)
+    
     res.status(200).json({ 
       totalPages: paginatedJson.length + 1,
       currentPage: page,
       results: paginatedJson[page]
     })
   } else {
-    res.status(200).json({ json})
+    res.status(200).json({ json })
   }
 
   
