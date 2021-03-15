2ff865e2916b6ccf0f1f1dea6425b1b3455bc095
benwaples <benwaples@gmail.com>
2021-03-14 19:55:11 -0700

    procy api

--- pages/api/starwars.ts
+++ pages/api/starwars.ts
@@ -1,3 +1,9 @@
-export default (req, res) => {
-  res.status(200).json({ message: 'you made it to the starwars api proxy'})
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const request = await fetch('https://akabab.github.io/starwars-api/api/all.json')
+  const json = await request.json() 
+
+
+  res.status(200).json({ json })
 }
