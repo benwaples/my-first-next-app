f831d4e951f61b182765c5d9928044b0cfc23ce8
benwaples <benwaples@gmail.com>
2021-03-15 13:45:17 -0700

    pagination for pages api

--- pages/api/starwars/index.ts
+++ pages/api/starwars/index.ts
@@ -1,9 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { makePages } from '../../../utils/utils'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+  const page = Number(req.query.page)
+  const perPage = Number(req.query.perPage)
+  console.log(page, perPage);
+  
+  
   const request = await fetch('https://akabab.github.io/starwars-api/api/all.json')
   const json = await request.json() 
 
+  if(page) {
+    const paginatedJson = makePages(json, perPage)
 
-  res.status(200).json({ json })
+    res.status(200).json({ 
+      totalPages: paginatedJson.length + 1,
+      currentPage: page,
+      results: paginatedJson[page]
+    })
+  } else {
+    res.status(200).json({ json})
+  }
+
+  
 }
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,5 @@
+import { CharacterTypes } from "./types"
+
 export function inaccuratePronouns(genderAccordingToWookiePediea: string): string[] {
   return genderAccordingToWookiePediea === 'male' ?
   ['he','him','his']
@@ -7,4 +9,20 @@ export function inaccuratePronouns(genderAccordingToWookiePediea: string): strin
 
 export function capitalize(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+export function makePages(arr: CharacterTypes[], perPage = 10): CharacterTypes[][] {
+  const pages = [];
+  
+  while(arr.length) {
+    if(arr.length >= perPage) {
+      pages.push(arr.splice(0, perPage))
+    } else {
+      pages.push(arr.slice())
+      arr.length = 0
+    }
+    
+  }
+  
+  return pages;
 }
