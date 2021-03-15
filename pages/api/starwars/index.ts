5642b94577e64bc3024e9943cc4cf62462e32fbc
benwaples <benwaples@gmail.com>
2021-03-15 14:00:20 -0700

    fix current page

--- pages/api/starwars/index.ts
+++ pages/api/starwars/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { makePages } from '../../../utils/utils'
+const URL = 'http://localhost:3000'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
@@ -11,11 +12,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if(page) {
     const paginatedJson = perPage ? makePages(json, perPage) : makePages(json)
-    
+
     res.status(200).json({ 
       totalPages: paginatedJson.length + 1,
       currentPage: page,
-      results: paginatedJson[page]
+      nextPage: `${URL}/api/starwars?page=${page + 1}&perPage=${perPage}`,
+      prevPage: `${URL}/api/starwars?page=${page !== 1 ? page - 1 : page }&perPage=${perPage}`,
+      results: paginatedJson[page - 1]
     })
   } else {
     res.status(200).json({ json })
