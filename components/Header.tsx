8457f308aa16729d3a2c12948cb67137c057a607
benwaples <benwaples@gmail.com>
2021-03-28 22:20:06 -0700

    pagenation endpoint

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ function Header(): JSX.Element {
 
   return (
     <header className={styles.header}>
-      <h1>Star Wars Search</h1>
+      <h1>Star Wars Proxy-API</h1>
       <a href='/characters'>HOME</a>
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,20 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      
+      <header>
+        <h1>
+          Welcome to the Starwars Proxy API!
+        </h1>
+      </header>
+      <main>
+        This are not the endpoints you are looking for (jk yes they are)
+
+        <section>
+          <h3>GET characters</h3>
+          returns json of all characters
+          <h3>GET characters?page={'{number}'}&perPage={'{number}'}</h3>
+        </section>
+      </main>
     </div>
   )
 }
